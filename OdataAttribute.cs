1b1cd360b6991647f400ab8201e42b364e10526c
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2020-05-27 15:02:52 +0300

    Add project files.

---
+++ OdataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.OData;
+
+namespace ODataOffsetProblemPOC
+{
+    public class ODataAttribute : EnableQueryAttribute
+    {
+        public ODataAttribute()
+            : base()
+
+        {
+            PageSize = 1;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+
+namespace ODataOffsetProblemPOC
+{
+    public class Startup
+    {
+        private const string ODATA_ROUTE_PREFIX = "odata";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddOData();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.MaxTop(10).Expand().Filter().OrderBy().SkipToken();
+                routeBuilder.MapODataServiceRoute(ODATA_ROUTE_PREFIX, ODATA_ROUTE_PREFIX, GetEdmModel());
+            });
+        }
+
+        private IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder
+               .EntitySet<WeatherForecast>("WeatherForecast")
+               .EntityType.HasKey(k => new { k.DateCEST });
+
+            return builder.GetEdmModel();
+        }
+    }
+}
